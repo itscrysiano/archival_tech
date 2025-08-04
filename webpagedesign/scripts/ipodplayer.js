a7bd67e9d1d119b63dab4afda6319e0c30380599
itscrysiano <crystalanokam@gmail.com>
2025-08-04 20:16:32 +0100

    iPod positioning

--- webpagedesign/scripts/ipodplayer.js
+++ webpagedesign/scripts/ipodplayer.js
@@ -18,11 +18,13 @@
   // Scene and camera
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-  camera.position.set(0, 1, 4);
+  camera.position.set(0, 0, 10.000);
+  camera.rotation.set(0, 0, 0)
 
   // OrbitControls
   const controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.target.set(1, 2, 0);
+  camera.position.set(0, 0, 10)
+  controls.target.set(0, 0, 0);
   controls.update();
 
   // Lighting
@@ -32,11 +34,17 @@
   // Load model
   const loader = new THREE.GLTFLoader();
   loader.load(
-    'assets/models/ipod2.glb',
+    'assets/models/ipod3.glb',
     function(gltf) {
       loadedModel = gltf.scene;
+
+    const box = new THREE.Box3().setFromObject(loadedModel);
+    const center = box.getCenter(new THREE.Vector3());
+    loadedModel.position.sub(center);
+
       scene.add(loadedModel);
     },
+    
     undefined,
     function(error) {
       console.error('Model loading error:', error);
